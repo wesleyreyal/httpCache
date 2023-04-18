e2dfab1f25229f78984747bc2cbca0288bd98ad4
REYAL Wesley <reyal.wesley@gmail.com>
2023-04-18 16:20:29 +0200

    feat: adding zentruck foundry fixture
    Adding fixtures to all the entities

---
+++ api/src/DataFixtures/ConfigurationFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\ConfigurationFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ConfigurationFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        ConfigurationFactory::createMany(10);
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            DomainFixtures::class,
+        ];
+    }
+}
