749e0b587243b83f9a4008bd26d50a73094b3d1f
REYAL Wesley <reyal.wesley@gmail.com>
2023-04-18 14:14:49 +0200

    feat: api adding entities user domain configuration

---
+++ api/src/Entity/Configuration.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ConfigurationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConfigurationRepository::class)]
+#[ApiResource]
+class Configuration
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $zone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $configuration = null;
+
+    #[ORM\Column(length: 39)]
+    private ?string $ip = null;
+
+    #[ORM\ManyToOne(inversedBy: 'configurations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Domain $domain = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getZone(): ?string
+    {
+        return $this->zone;
+    }
+
+    public function setZone(string $zone): self
+    {
+        $this->zone = $zone;
+
+        return $this;
+    }
+
+    public function getConfiguration(): ?string
+    {
+        return $this->configuration;
+    }
+
+    public function setConfiguration(string $configuration): self
+    {
+        $this->configuration = $configuration;
+
+        return $this;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function setIp(string $ip): self
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getDomain(): ?Domain
+    {
+        return $this->domain;
+    }
+
+    public function setDomain(?Domain $domain): self
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Domain.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\DomainRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DomainRepository::class)]
+#[ApiResource]
+class Domain
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $dns = null;
+
+    #[ORM\Column]
+    private ?bool $valid = null;
+
+    #[ORM\ManyToOne(inversedBy: 'domains')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    #[ORM\OneToMany(mappedBy: 'domain', targetEntity: Configuration::class, orphanRemoval: true)]
+    private Collection $configurations;
+
+    public function __construct()
+    {
+        $this->configurations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDns(): ?string
+    {
+        return $this->dns;
+    }
+
+    public function setDns(string $dns): self
+    {
+        $this->dns = $dns;
+
+        return $this;
+    }
+
+    public function isValid(): ?bool
+    {
+        return $this->valid;
+    }
+
+    public function setValid(bool $valid): self
+    {
+        $this->valid = $valid;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Configuration>
+     */
+    public function getConfigurations(): Collection
+    {
+        return $this->configurations;
+    }
+
+    public function addConfiguration(Configuration $configuration): self
+    {
+        if (!$this->configurations->contains($configuration)) {
+            $this->configurations->add($configuration);
+            $configuration->setDomain($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConfiguration(Configuration $configuration): self
+    {
+        if ($this->configurations->removeElement($configuration)) {
+            // set the owning side to null (unless already changed)
+            if ($configuration->getDomain() === $this) {
+                $configuration->setDomain(null);
+            }
+        }
+
+        return $this;
+    }
+}
