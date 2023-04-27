484338687506d0f30c2b5c348905e81172e65696
REYAL Wesley <reyal.wesley@gmail.com>
2023-04-27 11:53:11 +0200

    feat: project dependance
    setup project,dependance update

--- pwa/tailwind.config.js
+++ pwa/tailwind.config.js
@@ -19,13 +19,20 @@ module.exports = {
         24: "96px"
       },
       colors: {
+        eggshell: "#F3FAFF",
+        rich_black: "#01161E",
+        midnight_green: "#124559",
+        payne_gray:"#5F6C7B",
+        powder_blue:"#90B4CE",
+        argentinian_blue: "#3DA9FC",
+        bittersweet: "#F05D5E",
+        yellow: "#F6D800",
+        red: "#FF1313",
+        green: "#1AC421",
         cyan: {
           500: "#46b6bf",
           700: "#0f929a",
           200: "#bceff3"
-        },
-        red: {
-          500: "#ee4322"
         },
         black: "#1d1e1c",
         white: "#ffffff",
