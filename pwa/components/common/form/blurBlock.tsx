c6641f8878cff9f61774279de2ea0dee69bdc3d4
REYAL Wesley <reyal.wesley@gmail.com>
2023-05-09 16:48:47 +0200

    fix: component blurBlock

--- pwa/components/common/form/blurBlock.tsx
+++ pwa/components/common/form/blurBlock.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 type blurBlockProps = {
-  children: React.ReactNode;
+  className: string;
 };
-export const BlurBlock: React.FC<blurBlockProps> = ({ children }) => (
-  <div className="backdrop-blur-md bg-white/20 w-fit rounded-2xl">{children}</div>
+export const BlurBlock: React.FC<PropsWithChildren<blurBlockProps>> = ({ children, className }) => (
+  <div className={`backdrop-blur-md bg-white/20 w-fit rounded-2xl ${className}`}>{children}</div>
 );
