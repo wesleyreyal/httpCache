36d7c24d4731b4bdec2e601cf6ff39e17327103f
REYAL Wesley <reyal.wesley@gmail.com>
2023-05-16 20:51:42 +0200

    feat: component dropdown + textarea

--- pwa/components/common/input/index.ts
+++ pwa/components/common/input/index.ts
@@ -1,3 +1,5 @@
 export * from './input';
 export * from './inlineInput';
 export * from './switch';
+export * from './dropdown';
+export * from './textarea';
---
+++ pwa/components/common/input/textarea.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type textAreaType = {
+  text: string;
+} & React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
+export const Textarea: React.FC<textAreaType> = ({ text, ...props }) => {
+  return (
+    <textarea
+      className="w-full h-96 border-1 border-neutral-400 border-opacity-30 rounded-md  "
+      defaultValue={text}
+      {...props}
+    />
+  );
+};
