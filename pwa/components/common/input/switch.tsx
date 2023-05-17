32520e2025d18594b9041682c1adbbd3aa958183
REYAL Wesley <reyal.wesley@gmail.com>
2023-05-17 15:26:39 +0200

    fix: component switch
    using daisy ui

--- pwa/components/common/input/switch.tsx
+++ pwa/components/common/input/switch.tsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 type switchProps = {
   defaultChecked?: boolean;
+  onChange?: (value: string) => void;
 };
 
 export const Switch: React.FC<switchProps> = ({ defaultChecked = false }) => {
   const [checked, setChecked] = useState(defaultChecked);
-
-  return (
-    <div
-      className={`md:w-14 md:h-7 w-12 h-6 flex items-center border-2 rounded-full p-1 cursor-pointer ${
-        !checked ? 'border-powder_blue' : 'border-argentinian_blue'
-      }`}
-      onClick={() => setChecked(!checked)}
-    >
-      <div
-        className={` md:w-5 md:h-5 h-4 w-4 rounded-full shadow-md transform duration-300 ease-in
-        ${!checked ? 'bg-powder_blue' : 'bg-argentinian_blue transform translate-x-6'}`}
-      ></div>
-    </div>
-  );
+  return <input type="checkbox" className="toggle toggle-info" onClick={() => setChecked(!checked)} defaultChecked={checked} />;
 };
