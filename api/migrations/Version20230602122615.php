c1fe7a49df6b183780bd4fe12a8734f452969618
REYAL Wesley <reyal.wesley@gmail.com>
2023-06-02 14:30:52 +0200

    feat(api): user entity adding field token and activated

---
+++ api/migrations/Version20230602122615.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230602122615 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adding token and activated fields for User';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE "user" ADD token VARCHAR(530) DEFAULT NULL');
+        $this->addSql('ALTER TABLE "user" ADD activated BOOLEAN NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" DROP token');
+        $this->addSql('ALTER TABLE "user" DROP activated');
+    }
+}
