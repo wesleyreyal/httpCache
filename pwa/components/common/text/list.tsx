bb9c11c0c601116015d3a069b39875a08542fcd6
REYAL Wesley <reyal.wesley@gmail.com>
2023-06-13 15:45:10 +0200

    fix(pwa): List component adding items props

--- pwa/components/common/text/list.tsx
+++ pwa/components/common/text/list.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
 
-type listPropsType = React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>;
-export const List: React.FC<listPropsType> = ({ ...props }) => <ul {...props}>{props.children}</ul>;
+type listPropsType = {
+  items: ReadonlyArray<string>;
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>;
+
+export const List: React.FC<listPropsType> = ({ items, ...props }) => (
+  <ul {...props}>{items && items.map((item, idx) => <li key={idx}>{item}</li>)}</ul>
+);
