aa26a30f989fdd76f3bd562a638ae3d0d8525a9d
REYAL Wesley <reyal.wesley@gmail.com>
2023-06-14 12:45:25 +0200

    fix(pwa): landing page layout

--- pwa/components/common/card/index.tsx
+++ pwa/components/common/card/index.tsx
@@ -5,7 +5,7 @@ export type cardProps = {
   text: string;
 };
 export const Card: React.FC<PropsWithChildren<cardProps>> = ({ children, text, title }) => (
-  <div className="card w-96 bg-neutral-50 shadow-md rounded-xl p-4 h-64 flex items-center justify-center">
+  <div className="card w-72 bg-neutral-50 shadow-md rounded-xl p-4 h-64 flex items-center justify-center h-full">
     <div className="card-body text-center flex flex-col gap-4 items-center">
       {children}
       <h2 className="card-title font-bold font-sans text-xl">{title}</h2>
--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -6,17 +6,19 @@ import { BaseButton, OutlinedButton } from 'components/common/button';
 import { useRouter } from 'next/navigation';
 import { ROUTES } from 'routes';
 import { CardList } from 'components/list/card';
-import Image from 'next/image';
 import map from 'public/images/map.png';
 
 const Home: NextPage = () => {
   const { push } = useRouter();
   return (
     <>
-      <Image src={map} width={1000000} height={1000000} className="absolute" alt="background world map" />
-      <section className="w-full flex flex-col items-center gap-y-44">
-        <div className="m-0 justify-center">
-          <Blur className="flex flex-col p-4 items-center max-w-screen-sm gap-y-10 py-6 px-12">
+      <div
+        className="absolute h-screen w-screen bg-center bg-cover top-0"
+        style={{ backgroundImage: `url(${map.src})` }}
+      />
+      <div className="w-full flex flex-col items-center">
+        <div className="flex justify-center items-center h-screen w-screen">
+          <Blur className="flex flex-col p-4 items-center max-w-screen-sm gap-y-10 py-6 px-12 h-fit shadow-xl">
             <Title title="Say hello to Souin !" />
             <p className="text-justify">
               Souin is an intuitive HTTP cache management application that enables servers to cache web data, improving
@@ -28,27 +30,29 @@ const Home: NextPage = () => {
           </Blur>
         </div>
 
-        <div className="w-full max-w-screen-xl flex flex-col gap-y-12">
-          <Title title="Souin is better" />
-          <CardList />
-        </div>
+        <div className="flex flex-col items-center gap-y-20">
+          <div className="w-full max-w-screen-lg flex flex-col gap-y-12">
+            <Title title="Souin is better" />
+            <CardList />
+          </div>
 
-        <div className="flex flex-col items-center max-w-screen-sm mx-12 lg:max-w-screen-lg gap-y-10">
-          <Title title="Open-source" />
-          <p className="text-justify">
-            Souin is built for the community, by the community. Its code is totally open-source available on the github
-            repository{' '}
-            <a href="https://github.com/darkweak/souin" target="_blank">
-              github.com/darkweak/souin
-            </a>
-            . Everyone can access, audit and explore the code. Feel free to open a PR or issues if you think some parts
-            are not working as expected, if you encounter some troubles to configure it or if the doc is not clear
-            enough. There are no hidden part, or enterprise edition because it doesn't make sense to make money on the
-            back of the contributors and all features in Souin will stay free forever.
-          </p>
-          <OutlinedButton className="btn-outline" icon="github" text="Join the project" variant="ghost" />
+          <div className="flex flex-col items-center max-w-screen-sm mx-12 lg:max-w-screen-lg gap-y-6">
+            <Title title="Open-source" />
+            <p className="text-justify">
+              Souin is built for the community, by the community. Its code is totally open-source available on the
+              github repository{' '}
+              <a href="https://github.com/darkweak/souin" target="_blank">
+                github.com/darkweak/souin
+              </a>
+              . Everyone can access, audit and explore the code. Feel free to open a PR or issues if you think some
+              parts are not working as expected, if you encounter some troubles to configure it or if the doc is not
+              clear enough. There are no hidden part, or enterprise edition because it doesn't make sense to make money
+              on the back of the contributors and all features in Souin will stay free forever.
+            </p>
+            <OutlinedButton className="btn-outline" icon="github" text="JOIN THE PROJECT" variant="ghost" />
+          </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
